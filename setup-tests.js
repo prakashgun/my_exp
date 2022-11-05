85ae7bbfac88910e8eb69cbdabef7b0d4d08ae3a
prakashgun <mail.prakash.india@gmail.com>
2022-11-05 13:53:43 +0530

    Setup test config

---
+++ setup-tests.js
@@ -0,0 +1,30 @@
+import MockAsyncStorage from 'mock-async-storage'
+import 'react-native-gesture-handler/jestSetup'
+ 
+const mockImpl = new MockAsyncStorage()
+jest.mock('@react-native-async-storage/async-storage', () => mockImpl)
+
+jest.mock("@react-navigation/native", () => {
+    const actualNav = jest.requireActual("@react-navigation/native")
+    return {
+      ...actualNav,
+      useNavigation: () => ({
+        navigate: jest.fn(),
+        dispatch: jest.fn(),
+      }),
+      useIsFocused: ()=>{}
+    }
+  })
+
+  // jest.mock('react-native-reanimated', () => {
+  //   const Reanimated = require('react-native-reanimated/mock');
+  
+  //   // The mock for `call` immediately calls the callback which is incorrect
+  //   // So we override it with a no-op
+  //   Reanimated.default.call = () => {};
+  
+  //   return Reanimated;
+  // })
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
