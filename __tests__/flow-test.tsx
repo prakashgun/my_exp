242501211fb34b7f78a68335d3e864f3eca2f4bb
prakashgun <mail.prakash.india@gmail.com>
2022-11-12 09:29:49 +0530

    Test with naviagtion added

--- __tests__/flow-test.tsx
+++ __tests__/flow-test.tsx
@@ -1,9 +1,9 @@
-import {render} from '@testing-library/react-native'
+import {render, screen, fireEvent, waitFor} from '@testing-library/react-native'
 import { act } from 'react-test-renderer'
 import App from '../App'
 
 describe('Main flow', ()=>{
-    it('Shows correct title',()=>{
+    test('Shows correct title',()=>{
         jest.useFakeTimers()
         const {getByText} = render(<App />)
 
@@ -14,4 +14,17 @@ describe('Main flow', ()=>{
         expect(getByText('Accounts')).toBeTruthy()
         expect(getByText('Add')).toBeTruthy()
     })
+
+    test('Go to add screen', async()=>{
+        jest.useFakeTimers()
+        const {getByText} = render(<App />)
+
+        act(()=>{
+            jest.runAllTimers()
+        })
+
+        fireEvent(await getByText('Add'), 'press')
+        expect(await getByText('Add Account')).toBeTruthy()
+    })
+
 })
