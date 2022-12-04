934aaccd20b65836c5c95ea7e549e661160f3417
prakashgun <mail.prakash.india@gmail.com>
2022-12-04 12:34:58 +0530

    react native testing library installed

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React, { useEffect } from 'react'
 import { createTables } from './src/common/dbQueries'
+import { Icon } from '@rneui/themed'
 
 const App = () => {
 
@@ -15,6 +16,9 @@ const App = () => {
   return (
     <View>
       <Text>App</Text>
+      <Icon
+        name='rowing' />
+
     </View>
   )
 }
